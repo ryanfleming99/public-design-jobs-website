9a22831a15e099fb2cee2b47bed2c157f6b9d872
Ryan Fleming <ryanfleming93@aol.com>
2024-10-06 17:39:28 +0100

    updated mailer

--- netlify/functions/sendWelcomeEmail.js
+++ netlify/functions/sendWelcomeEmail.js
@@ -23,7 +23,7 @@ exports.handler = async function(event, context) {
   const msg = {
     to: email,
     from: "ryan@publicdesignjobs.co.uk", // Your verified SendGrid email
-    templateId: "d-f7640723d0984adf984dfa36ccaefec8", // Your SendGrid template ID
+    templateId: "d-f04c548ce2bc4aeb9e0d8f4c49de529b", // Your SendGrid template ID
     dynamic_template_data: {
       email: email
     }
